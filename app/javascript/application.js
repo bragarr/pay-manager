b9f9ea6ca1a8d3f2eb44a47ba0b775962f786d4f
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2023-04-20 22:16:39 -0400

    feat: setting firebase and new private keys

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails";
 import "./controllers/application.js";
-import "./controllers/firebase.js";
+import "./controllers/profile_picture.js";
 import * as bootstrap from "bootstrap";
---
+++ app/javascript/controllers/profile_picture.js
@@ -0,0 +1,37 @@
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { storage } from "../context/firebase.js";
+
+const formulario = document.querySelector(".formulario");
+
+let profUrl = "";
+
+const handleUpload = (event) => {
+    event.preventDefault();
+
+    const file = event.target[0]?.files[0];
+    if (!file) return;
+
+    const storageRef = ref(storage, `images/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+            const progress =
+                (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        },
+        (error) => {
+            alert(error);
+        },
+        () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+                profUrl = url;
+            });
+        }
+    );
+};
+
+formulario.addEventListener("submit", () => {
+    handleUpload(event);
+});
+
