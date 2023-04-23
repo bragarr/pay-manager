509fe0e8ef24b66d3f9d773f697987dcbce7fd1f
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2023-04-23 19:54:55 -0400

    feat: full profile picture update

--- app/javascript/controllers/profile_picture.js
+++ app/javascript/controllers/profile_picture.js
@@ -1,15 +1,24 @@
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { storage } from "../context/firebase.js";
 
+const validImageTypes = ["image/gif", "image/jpeg", "image/png"];
 const formulario = document.querySelector(".formulario");
+const buttonSubmit = document.querySelector(".button-submit-picture");
+const containerPicture = document.querySelector(".profile-picture");
+const currentProfilePicture = document.querySelector(".current-profile-picture");
+const newProfilePicture = document.querySelector(".profile-url");
+const containerProgrsssBar = document.querySelector(".progress");
+const progressBar = document.querySelector(".progress-bar");
 
-let containerPicture = document.querySelector(".profile-picture");
 
 const handleUpload = (event) => {
 	event.preventDefault();
 
 	const file = event.target[0]?.files[0];
-	if (!file) return;
+	const fileType = file["type"];
+	if (!file || !validImageTypes.includes(fileType)) return;
+
+	containerProgrsssBar.classList.remove("d-none");
 
 	const storageRef = ref(storage, `images/${file.name}`);
 	const uploadTask = uploadBytesResumable(storageRef, file);
@@ -19,15 +28,18 @@ const handleUpload = (event) => {
 		(snapshot) => {
 			const progress =
 				(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+			progressBar.classList.add(`w-${progress}`);
 		},
 		(error) => {
 			alert(error);
 		},
 		() => {
+			if(progressBar.classList.contains("w-100")) {
+				containerProgrsssBar.classList.add("d-none");
+			}
 			getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-				const newProfilePicture = document.createElement("img");
-				newProfilePicture.src = url;
-				containerPicture.appendChild(newProfilePicture);
+				currentProfilePicture.src = url;
+				newProfilePicture.value = url;
 			});		
 		}
 	);           
